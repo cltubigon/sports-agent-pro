6f6ae9c0fed645e96d06ce5dac794da1250c33c3
cltubigon <60680621+cltubigon@users.noreply.github.com>
2023-04-12 02:21:48 +0800

    With Error

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -8,6 +8,8 @@ import {
   FaCommentDollar,
   FaRegGrinHearts,
   FaUserTie,
+  FaChevronLeft,
+  FaSortUp,
 } from "react-icons/fa"
 import { ThemeContext } from "../../contexts/themeContext"
 import { Link, useNavigate } from "react-router-dom"
@@ -16,18 +18,34 @@ import axios from "axios"
 import { AuthContext } from "../../contexts/AuthContext"
 
 const Signup = () => {
+  const [userType, setUserType] = useState(null)
+  const [isFirstStep, setIsFirstStep] = useState(true)
+  const [activateButton, setActivateButton] = useState(false)
+  const navigate = useNavigate()
+  
+  const handleUserTypeClick = (user) => {
+    setUserType(user)
+    setActivateButton((prev) => true)
+  }
+  console.log(userType)
+
   const [credentials, setCredentials] = useState({
     username: null,
+    first_name: null,
+    first_name: null,
     email: null,
     password: null,
+    confirm_password: null,
+    phone: null,
+    user_type: null,
   })
+  console.log(credentials)
   const { loading, error, dispatch } = useContext(AuthContext)
 
   const handleChange = (e) => {
     setCredentials((prev) => ({ ...prev, [e.target.name]: e.target.value }))
     dispatch({ type: "LOGIN_FAILURE", payload: null })
   }
-  const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -37,6 +55,13 @@ const Signup = () => {
       url: "http://localhost:8000/api/auth/register",
       headers: { "Content-Type": "application/json" },
       data: {
+        username: credentials.first_name + credentials.last_name + Math.floor(Math.random() * 100),
+        first_name: credentials.first_name,
+        last_name: credentials.last_name,
+        email: credentials.email,
+        phone: credentials.email,
+        password: credentials.password,
+        user_type: userType,
       },
     }
     axios
@@ -46,6 +71,7 @@ const Signup = () => {
         navigate("/")
       })
       .catch(function (error) {
+        console.log(error)
         switch (true) {
           case error.response.data.message.includes("dup key: { username:"):
             dispatch({
@@ -65,7 +91,7 @@ const Signup = () => {
             })
             break
           default:
-            console.log("Member of JLA")
+            console.log("Switch Success")
         }
       })
   }
@@ -102,13 +128,6 @@ const Signup = () => {
       id: 5,
     },
   ]
-  const [userType, setUserType] = useState(null)
-  const [isFirstStep, setIsFirstStep] = useState(true)
-
-  const handleUserTypeClick = (user) => {
-    setUserType(user)
-  }
-  console.log(userType)
 
   const darkTheme = useContext(ThemeContext) //Theme Styling
   const themeStyles = !darkTheme ? "dark-theme" : "light-theme" // End Theme
@@ -121,43 +140,95 @@ const Signup = () => {
           themeStyles
         }
       >
-        <h2>Select Account Type</h2>
         <div className="">
           <form onSubmit={handleSubmit}>
-            {isFirstStep && <div className="step-1">
-              {typeOfUsers?.map((user) => {
-                return (
-                  <div
-                    onClick={() => handleUserTypeClick(user.type)}
-                    className={
-                      userType == user.type
-                        ? "flex pt-3 pb-3 my-8 border-[1px] border-[#ccc] cursor-pointer rounded-md selected"
-                        : "flex pt-3 pb-3 my-8 border-[1px] border-[#ccc] cursor-pointer rounded-md "
-                    }
-                    key={user.id}
-                  >
-                    <div className="flex items-center w-[20%] justify-center text-[#89949F]">
-                      {user.icon}
+            {isFirstStep && (
+              <div className="step-1">
+                <h2 className="text-[28px] font-semibold text-center mb-6">
+                  Select Account Type
+                </h2>
+                {typeOfUsers?.map((user) => {
+                  return (
+                    <div
+                      onClick={() => handleUserTypeClick(user.type)}
+                      className={
+                        userType == user.type
+                          ? "flex pt-3 pb-3 my-8 border-[1px] border-[#ccc] cursor-pointer rounded-md selected"
+                          : "flex pt-3 pb-3 my-8 border-[1px] border-[#ccc] cursor-pointer rounded-md "
+                      }
+                      key={user.id}
+                    >
+                      <div className="flex items-center w-[20%] justify-center text-[#89949F]">
+                        {user.icon}
+                      </div>
+                      <div className="flex flex-col w-[80%]">
+                        <p className="text-base font-bold">{user.type}</p>
+                        <p className="text-sm font-light">{user.desc}</p>
+                      </div>
                     </div>
-                    <div className="flex flex-col w-[80%]">
-                      <p className="text-base font-bold">{user.type}</p>
-                      <p className="text-sm font-light">{user.desc}</p>
-                    </div>
-                  </div>
-                )
-              })}
-              <button
-                onClick={(e) => {
-                  e.preventDefault()
-                  setIsFirstStep(false)
-                }}
-              >
-                Continue
-              </button>
-            </div>}
+                  )
+                })}
+                <button
+                  className={
+                    !activateButton && "pointer-events-none opacity-50"
+                  }
+                  onClick={(e) => {
+                    e.preventDefault()
+                    setIsFirstStep((prev) => (prev = false))
+                  }}
+                >
+                  Continue
+                </button>
+              </div>
+            )}
             {!isFirstStep && (
               <div className="step-2">
+                <div>
+                  <FaChevronLeft
+                    onClick={() => {
+                      setIsFirstStep((prev) => (prev = true))
+                    }}
+                    className="text-2xl cursor-pointer"
+                  />
+                </div>
+                <h2 className="text-[28px] font-semibold text-center mb-6">
+                  Create your account
+                </h2>
                 <div className="flex flex-col p-1">
+                  <label htmlFor="first_name" className="text-sm mb-[6px]">
+                    <span className="text-[red]">*</span> First Name
+                  </label>
+                  <input
+                    onChange={handleChange}
+                    required
+                    type="text"
+                    placeholder="First Name"
+                    // value=""
+                    name="first_name"
+                    minLength={5}
+                    // pattern="^[a-zA-Z0-9]{5,16}$"
+                    className={
+                      darkTheme
+                        ? "mb-3 p-2 mb-[20px]"
+                        : "mb-3 p-2 mb-[20px] text-[var(--primary-color)]"
+                    }
+                  />
+                  <label htmlFor="last_name" className="text-sm mb-[6px]">
+                    <span className="text-[red]">*</span> Last Name
+                  </label>
+                  <input
+                    onChange={handleChange}
+                    required
+                    type="text"
+                    placeholder="Last Name"
+                    // value=""
+                    name="last_name"
+                    className={
+                      darkTheme
+                        ? "mb-3 p-2 mb-[20px]"
+                        : "mb-3 p-2 mb-[20px] text-[var(--primary-color)]"
+                    }
+                  />
                   <label htmlFor="email" className="text-sm mb-[6px]">
                     <span className="text-[red]">*</span> Email address
                   </label>
@@ -187,10 +258,15 @@ const Signup = () => {
                     // value=""
                     name="password"
                     className={
-                      darkTheme ? "p-2 mb-[20px]" : "p-2 mb-[20px] text-[var(--primary-color)]"
+                      darkTheme
+                        ? "p-2 mb-[20px]"
+                        : "p-2 mb-[20px] text-[var(--primary-color)]"
                     }
                   />
-                  <label htmlFor="confirm-password" className="text-sm mb-[6px]">
+                  <label
+                    htmlFor="confirm-password"
+                    className="text-sm mb-[6px]"
+                  >
                     <span className="text-[red]">*</span> Confirm Password
                   </label>
                   <input
@@ -201,13 +277,25 @@ const Signup = () => {
                     // value=""
                     name="confirm-password"
                     className={
-                      darkTheme ? "p-2 mb-[20px]" : "p-2 mb-[20px] text-[var(--primary-color)]"
+                      darkTheme
+                        ? "p-2 mb-[20px]"
+                        : "p-2 mb-[20px] text-[var(--primary-color)]"
                     }
                   />
                 </div>
                 <p className="flex flex-col justify-center text-sm mt-[19px]">
-            I agree to the Opendorse Terms of use and Privacy policy
-            </p>  
+                  <div>
+                    <label forHTML="sports-agent-pro-gpdr">
+                      <input
+                        required
+                        title="Please agree to the GDPR terms and conditions."
+                        type="checkbox"
+                        name="sports-agent-pro-gpdr"
+                      />{" "}
+                      I agree to the Opendorse Terms of use and Privacy policy
+                    </label>
+                  </div>
+                </p>
                 <input
                   className="mt-[30px] login-button text-xl bg-slate-600 w-full py-[10px] text-white cursor-pointer"
                   type="submit"
@@ -221,15 +309,19 @@ const Signup = () => {
           )}
           {loading && (
             <p className="text-green-600 p-3 text-center text-sm">
-              Signing in...
+              Creating your account...
             </p>
           )}
-          <p className="text-center text-sm mt-[35px] flex justify-center">
-            <span>
-              <FaHeadset className="mt-[4px] mr-2" />
-            </span>
-            Contact our support team
-          </p>
+          <div className="mt-4 flex flex-row gap-3 justify-center">
+            <p className="flex flex-col justify-center">
+              Don't have an account?
+            </p>{" "}
+            <button
+              className={darkTheme ? "bg-light-important" : "bg-dark-important"}
+            >
+              <Link to="/login">Login</Link>
+            </button>
+          </div>
         </div>
       </div>
     </div>
